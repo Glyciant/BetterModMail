2e4ea312b85d33291bf2fabf7bc3e5f63f664002
Heep <contact.heep@gmail.com>
2016-04-20 17:54:10 +0100

    Minor fixes to default responses.

--- views/static/js/mail.js
+++ views/static/js/mail.js
@@ -66,105 +66,36 @@ $(document).ready(function(){
      $("#create-reply-" + id).val("")
    }
    else if (value === "Channel Advertisement") {
-     $("#create-reply-" + id).val(`
-Hey there,
-
-Direct channel advertisements are a violation of rule #3 and are not allowed on the Sub. Instead, I suggest you [create a channel intro over at TwitchDB.tv](http://www.twitchdb.tv/createintro), a site managed by the /r/Twitch mod team. Beyond the Sub, there are many ways to promote your channel and your brand. These are [some](https://www.reddit.com/r/Twitch/comments/3clebh/getting_noticed_on_twitch/) [guides](https://www.reddit.com/r/Twitch/comments/3drowh/using_social_media_effectively/) from around the Sub.
-
-Thanks for messaging us!
-      `)
+     $("#create-reply-" + id).val("Hey there,\n \nDirect channel advertisements are a violation of rule #3 and are not allowed on the Sub. Instead, I suggest you [create a channel intro over at TwitchDB.tv](http://www.twitchdb.tv/createintro), a site managed by the /r/Twitch mod team. Beyond the Sub, there are many ways to promote your channel and your brand. These are [some](https://www.reddit.com/r/Twitch/comments/3clebh/getting_noticed_on_twitch/) [guides](https://www.reddit.com/r/Twitch/comments/3drowh/using_social_media_effectively/) from around the Sub.\n \nThanks for messaging us!")
    }
    else if (value === "Personal Help Response") {
-     $("#create-reply-" + id).val(`
-Hey there,
-
-You have sent us a modmail message, however we are unable to help individual users with non-subreddit related problems. Instead, I suggest clicking the '[Submit New Topic](https://www.reddit.com/r/Twitch/submit?selftext=true)' button, on the subreddit sidebar - and let the community help you. Please make sure to read the rules, and check your post follows them. It would also help if you can provide as much information as possbile.
-
-Thanks!
-     `)
+     $("#create-reply-" + id).val("Hey there,\n \nYou have sent us a modmail message, however we are unable to help individual users with non-subreddit related problems. Instead, I suggest clicking the '[Submit New Topic](https://www.reddit.com/r/Twitch/submit?selftext=true)' button, on the subreddit sidebar - and let the community help you. Please make sure to read the rules, and check your post follows them. It would also help if you can provide as much information as possbile.\n \nThanks!")
    }
    else if (value === "Ad Request Form") {
-     $("#create-reply-" + id).val(`
-Hey there,
-
-Please could you fill out [this form](https://docs.google.com/forms/d/1Rpf4z3QNm0FWIi4b1wk4KWst3_KARvBuOTwsWoTMzlA/viewform)? It helps us to find out more information about your advertisement request and helps to improve our judgement on approving it. Ensure any links are included and you provide as much information as possible. Remember that posting your request before it is approved is against subreddit rule #3. Please note these approvals sometimes take a while, but feel free to ask again after 7 days.
-If you have any other issues, please let us know.
-
-Thanks!
-     `)
+     $("#create-reply-" + id).val("Hey there,\n \nPlease could you fill out [this form](http://requests.twithdb.tv/submit)? It helps us to find out more information about your advertisement request and helps to improve our judgement on approving it. Ensure any links are included and you provide as much information as possible. Remember that posting your request before it is approved is against subreddit rule #3. Please note these approvals sometimes take a while, but feel free to ask again after 7 days.\n \nIf you have any other issues, please let us know.\n \nThanks!")
    }
    else if (value === "Ad Request Approved") {
-     $("#create-reply-" + id).val(`
-Hey there,
-
-We have now had time to review the advertisement request you submitted, and have decided to allow your post.
-
-Thanks for your patience!
-     `)
+     $("#create-reply-" + id).val("Hey there,\n \nWe have now had time to review the advertisement request you submitted, and have decided to allow your post.\n \nThanks for your patience!"     )
    }
    else if (value === "Ad Request Rejected") {
-     $("#create-reply-" + id).val(`
-Hey there,
-
-We have now had time to review the advertisement request you submitted. However, we do not currently think it would be suitable to allow you to post your thread at this time.
-
-Thanks for your patience and understanding!
-     `)
+     $("#create-reply-" + id).val("Hey there,\n \nWe have now had time to review the advertisement request you submitted. However, we do not currently think it would be suitable to allow you to post your thread at this time.\n \nThanks for your patience and understanding!")
    }
    else if (value === "Karma Filter Query Approve") {
-     $("#create-reply-" + id).val(`
-Hey there,
-
-Sorry to hear this has been happening to you. We have our AutoModerator bot configured to automatically remove any comment/thread posted by a user whose karma falls below a certain level. This is generally effective at removing some trolls from the subreddit, though some good users do get caught up in it occassionally. It would appear that is what has happened here.
-
-Since your reddit history shows you were downvoted unfairly, I have added you to the filter's whitelist and re-approved any comments/posts that were accidentally removed. AutoMod should leave you alone now.
-
-Thanks for messaging us!
-     `)
+     $("#create-reply-" + id).val("Hey there,\n \nSorry to hear this has been happening to you. We have our AutoModerator bot configured to automatically remove any comment/thread posted by a user whose karma falls below a certain level. This is generally effective at removing some trolls from the subreddit, though some good users do get caught up in it occassionally. It would appear that is what has happened here.\n \nSince your reddit history shows you were downvoted unfairly, I have added you to the filter's whitelist and re-approved any comments/posts that were accidentally removed. AutoMod should leave you alone now.\n \nThanks for messaging us!")
    }
    else if (value === "Karma Filter Query Rejection") {
-     $("#create-reply-" + id).val(`
-Hey there,
-
-We have our AutoModerator bot configured to automatically remove any comment/thread posted by a user whose karma falls below a certain level. Your reddit history suggests that you are not suitable to be put in this filter's whitelist, and AutoMod will therefore continue to remove your comments/posts.
-
-Thanks for understanding.
-     `)
+     $("#create-reply-" + id).val("Hey there,\n \nWe have our AutoModerator bot configured to automatically remove any comment/thread posted by a user whose karma falls below a certain level. Your reddit history suggests that you are not suitable to be put in this filter's whitelist, and AutoMod will therefore continue to remove your comments/posts.\n \nThanks for understanding.")
    }
    else if (value === "Shadowbanned") {
-     $("#create-reply-" + id).val(`
-Hey there,
-
-It appears that you have been shadowbanned from the entirety of reddit. This means that only you and community moderators are able to see what you comment or post. We have no insight into why you are shadowbanned, so it's best to discuss and appeal it with reddit admins. For more information, you might want to look at [this guide](https://www.reddit.com/r/ShadowBan/comments/1vyaa2/a_guide_to_getting_unshadowbanned_sticky_maybe/). In the meantime, I will approve any non-rule-breaking posts/comments you submitted.
-
-Thanks for messaging us!
-     `)
+     $("#create-reply-" + id).val("Hey there,\n \nIt appears that you have been shadowbanned from the entirety of reddit. This means that only you and community moderators are able to see what you comment or post. We have no insight into why you are shadowbanned, so it's best to discuss and appeal it with reddit admins. For more information, you might want to look at [this guide](https://www.reddit.com/r/ShadowBan/comments/1vyaa2/a_guide_to_getting_unshadowbanned_sticky_maybe/). In the meantime, I will approve any non-rule-breaking posts/comments you submitted.\n \nThanks for messaging us!")
    }
    else if (value === "Global Moderator Application") {
-     $("#create-reply-" + id).val(`
-Hey there,
-
-We are a community-run unofficial subreddit and therefore are in no position to provide users this position. Furthermore, there is no set application process at this current time. I suggest reading [this guide](https://www.reddit.com/r/Twitch/comments/3xwwj2/how_to_become_a_twitch_global_moderatoradmin_v20/).
-
-Thanks for messaging us!
-     `)
+     $("#create-reply-" + id).val("Hey there,\n \nWe are a community-run unofficial subreddit and therefore are in no position to provide users this position. Furthermore, there is no set application process at this current time. I suggest reading [this guide](https://www.reddit.com/r/Twitch/comments/3xwwj2/how_to_become_a_twitch_global_moderatoradmin_v20/).\n \nThanks for messaging us!")
    }
    else if (value === "Reporting Users") {
-     $("#create-reply-" + id).val(`
-Hey there,
-
-We are a community-run unofficial subreddit and therefore are in no position to manage Twitch moderation. You should instead use the [report feature](http://help.twitch.tv/customer/portal/articles/725568-how-to-file-a-report), which will allow the problem to be reviewed by an on-duty admin and for action to be taken where necessary. Make sure to include as much evidence and detail as possible - [this guide](https://www.reddit.com/r/Twitch/comments/3trrov/report_guide_and_faq/) provides tips on how to do so.
-
-Thanks for messaging us!
-     `)
+     $("#create-reply-" + id).val("Hey there,\n \nWe are a community-run unofficial subreddit and therefore are in no position to manage Twitch moderation. You should instead use the [report feature](http://help.twitch.tv/customer/portal/articles/725568-how-to-file-a-report), which will allow the problem to be reviewed by an on-duty admin and for action to be taken where necessary. Make sure to include as much evidence and detail as possible - [this guide](https://www.reddit.com/r/Twitch/comments/3trrov/report_guide_and_faq/) provides tips on how to do so.\n \nThanks for messaging us!")
    }
    else if (value === "Flair Fixed") {
-     $("#create-reply-" + id).val(`
-Hey there,
-
-Your flair has now been restored. Please try to avoid clicking 'edit flair' on the sidebar as lots of people manage to remove their flair by doing so.
-
-Thanks for messaging us!
-     `)
+     $("#create-reply-" + id).val("Hey there,\n \nYour flair has now been restored. Please try to avoid clicking 'edit flair' on the sidebar as lots of people manage to remove their flair by doing so.\n \nThanks for messaging us!")
    }
 })
